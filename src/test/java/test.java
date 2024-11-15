5f802f8dec37d58e549f593eb0e241b3e5b38f89
eamun <eamongahran@gmail.com>
2024-11-15 17:38:30 +0000

    test

---
+++ src/test/java/test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+
+public class test {
+
+    @Test
+    void testSomething() {
+        // Your test logic here
+        System.out.println("This is a test method.");
+    }
+}
